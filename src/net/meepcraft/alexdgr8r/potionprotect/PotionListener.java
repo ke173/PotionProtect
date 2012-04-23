e1b3be62d8fd844725070dac97bca1fa43fd45cf
ke173 <minecraftkedalion@hmamail.com>
2012-04-23 15:33:42 -0300

    Update src/net/meepcraft/alexdgr8r/potionprotect/PotionListener.java

--- src/net/meepcraft/alexdgr8r/potionprotect/PotionListener.java
+++ src/net/meepcraft/alexdgr8r/potionprotect/PotionListener.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.potion.PotionEffect;
+import org.bukkit.entity.Entity
 
 public class PotionListener implements Listener {
 	
@@ -14,6 +15,12 @@ public class PotionListener implements Listener {
 		ThrownPotion potion = event.getPotion();
 		for (PotionEffect effect : potion.getEffects()) {
 			effect.getType().getId();
+			if(potion.inGround //Just found away to find the exact location - Kedalion
+			float findloc = potion.getLocation();
+			
+			return findloc;
+				
+			}
 		}
 	}
 
